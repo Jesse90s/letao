ec35bb7da115a441ef7cb9a9dcb0bc683b019d1d
Jesse90s <jessepinkman90s@gmail.com>
2018-09-03 20:07:26 +0800

    第二次提交

--- public/m/js/index.js
+++ public/m/js/index.js
@@ -29,9 +29,9 @@ Leteo.prototype = {
         mui('.mui-scroll-wrapper').scroll({
             deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
 
-            // scrollY: true, //是否竖向滚动
+            scrollY: true, //是否竖向滚动
             // scrollX: false, //是否横向滚动
-            // startX: 0, //初始化时滚动至x
+            startX: 0, //初始化时滚动至x
             // startY: 0, //初始化时滚动至y
             // indicators: true, //是否显示滚动条
             // deceleration:0.0006, //阻尼系数,系数越小滑动越灵敏
